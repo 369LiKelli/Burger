f6c458765d5ed608435dfe8abd992b97d21561c1
369LiKelli <118544087+369LiKelli@users.noreply.github.com>
2022-11-18 14:38:05 +0000

    Add files via upload

---
+++ order.js
@@ -0,0 +1,15 @@
+/*  set ORDER page Supersize alert  */
+function testIn(){
+	let get1 = document.getElementById("col3");
+	if(get1.value == 'option3'){
+		alert("Are you sure you wanna buy this size for your family?");
+	}
+			
+}
+	
+	
+	
+	
+	
+	
+	
